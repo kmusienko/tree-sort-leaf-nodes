20044a55b5305ca4b8645b0c52c0309a32157716
kmusienko <konstantin.musienko@sysgears.com>
2019-01-20 14:36:09 +0200

    Implement a method that sorts leaf nodes using specified traversal iterator and constant W

--- src/main/scala/TreeNode.scala
+++ src/main/scala/TreeNode.scala
@@ -1,4 +1,24 @@
+import TreeTraversalTypes.TraversalType
+
 import scala.collection.immutable.Queue
 
 case class TreeNode(nodes: Queue[TreeNode] = Queue.empty,
-                    leafList: SinglyLinkedList = SinglyLinkedList.empty)
+                    leafList: SinglyLinkedList = SinglyLinkedList.empty) {
+
+  private def sortLeafNodes(iterator: Iterator[TreeNode], w: Int): Unit = {
+    if (leafList.isEmpty) {
+      if (iterator.hasNext) {
+        val next = iterator.next
+        next.sortLeafNodes(iterator, w)
+      }
+    } else {
+      leafList.sort()
+      val removedValues = leafList.removeAfterSumMoreThanW(w)
+      if (iterator.hasNext) {
+        val nextNode = iterator.next()
+        nextNode.leafList.addAll(removedValues)
+        nextNode.sortLeafNodes(iterator, w)
+      }
+    }
+  }
+}
