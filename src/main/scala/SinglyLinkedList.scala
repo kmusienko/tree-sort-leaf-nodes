3713f1045a893a8cece49094cc2f4195f9fef02a
kmusienko <konstantin.musienko@sysgears.com>
2019-01-20 18:26:35 +0200

    Add scala-docs for SinglyLinkedList

--- src/main/scala/SinglyLinkedList.scala
+++ src/main/scala/SinglyLinkedList.scala
@@ -1,9 +1,24 @@
+/**
+  * Implementation of a singly linked list of integers.
+  */
 case class SinglyLinkedList() {
 
+  /**
+    * Head of the list.
+    */
   var head: Option[Node] = None
 
+  /**
+    * Size of the list
+    */
   var size: Int = 0
 
+  /**
+    * Represents a node of a singly linked list. Singly linked list consists of 'linked' nodes.
+    *
+    * @param value value that associated with this node
+    * @param next  link to the next node
+    */
   case class Node(var value: Int, var next: Option[Node] = None) {
     def print(): Unit = {
       println(value)
@@ -18,6 +33,11 @@ case class SinglyLinkedList() {
     addAll(elems)
   }
 
+  /**
+    * Adds a sequence of int elements to the singly linked list.
+    *
+    * @param elems elements to be added
+    */
   def addAll(elems: Seq[Int]): Unit = {
     elems.foreach {
       elem =>
@@ -28,6 +48,13 @@ case class SinglyLinkedList() {
     size += elems.size
   }
 
+  /**
+    * Iterates over the nodes and removes the rest of the nodes starting from a node when a sum of iterated node values
+    * is more than given constant W.
+    *
+    * @param w given constant
+    * @return list of removed node values
+    */
   def removeAfterSumMoreThanW(w: Int): List[Int] = {
     head match {
       case Some(node) =>
@@ -49,6 +76,12 @@ case class SinglyLinkedList() {
     }
   }
 
+  /**
+    * Removes all the nodes after a specified node.
+    *
+    * @param node node after which the rest of the linked list should be removed
+    * @return list of removed node values
+    */
   def removeAllAfter(node: Node): List[Int] = {
     val removedValues = toScalaList(fromNode = node.next)
     node.next = None
@@ -56,6 +89,12 @@ case class SinglyLinkedList() {
     removedValues
   }
 
+  /**
+    * Creates a Scala List with the values from this SinglyLinkedList with the same order.
+    *
+    * @param fromNode node from which to start copying values
+    * @return new scala list with copied values
+    */
   def toScalaList(fromNode: Option[Node] = head): List[Int] = {
     fromNode match {
       case Some(node) => List(node.value) ++ toScalaList(node.next)
@@ -63,16 +102,26 @@ case class SinglyLinkedList() {
     }
   }
 
+  /**
+    * Sorts the SinglyLinkedList in ascending order.
+    * Actually, it just calls 'mergeSort' method and overrides the head node.
+    */
   def sort(): Unit = {
     head = mergeSort(head)
   }
 
+  /**
+    * Creates a sorted SingleLinkedList in ascending order. Uses Merge Sort algorithm.
+    *
+    * @param maybeNode node to start sorting from
+    * @return sorted SingleLinkedList
+    */
   def mergeSort(maybeNode: Option[Node]): Option[Node] = {
     maybeNode.flatMap {
       node =>
         node.next match {
           case Some(_) =>
-            val maybeMiddle: Option[Node] = getMiddle(node)
+            val maybeMiddle: Option[Node] = findMiddle(node)
             val maybeNextOfMiddle: Option[Node] = maybeMiddle.flatMap(_.next)
 
             maybeMiddle.foreach(_.next = None)
@@ -80,32 +129,43 @@ case class SinglyLinkedList() {
             val maybeLeft = mergeSort(maybeNode)
             val maybeRight = mergeSort(maybeNextOfMiddle)
 
-            val sortedList: Option[Node] = sortedMerge(maybeLeft, maybeRight)
+            val sortedList: Option[Node] = mergeSorted(maybeLeft, maybeRight)
             sortedList
           case _ => Option(node)
         }
     }
   }
 
-  def sortedMerge(maybeNodeA: Option[Node], maybeNodeB: Option[Node]): Option[Node] = {
-    var result: Option[Node] = None
-    (maybeNodeA, maybeNodeB) match {
-      case (None, None) => None
+  /**
+    * Merges two sorted SingleLinkedLists into one.
+    *
+    * @param maybeLeft  left linked list
+    * @param maybeRight right linked list
+    * @return SingleLinkedList as a result of merging left and right lists
+    */
+  def mergeSorted(maybeLeft: Option[Node], maybeRight: Option[Node]): Option[Node] = {
+    (maybeLeft, maybeRight) match {
+      case (Some(nodeA), Some(nodeB)) if nodeA.value <= nodeB.value =>
+        val result = Some(nodeA)
+        result.get.next = mergeSorted(nodeA.next, Some(nodeB))
+        result
+      case (Some(nodeA), Some(nodeB)) =>
+        val result = Some(nodeB)
+        result.get.next = mergeSorted(Some(nodeA), nodeB.next)
+        result
       case (Some(nodeA), None) => Some(nodeA)
       case (None, Some(nodeB)) => Some(nodeB)
-      case (Some(nodeA), Some(nodeB)) =>
-        if (nodeA.value <= nodeB.value) {
-          result = Some(nodeA)
-          result.get.next = sortedMerge(nodeA.next, Some(nodeB))
-        } else {
-          result = Some(nodeB)
-          result.get.next = sortedMerge(Some(nodeA), nodeB.next)
-        }
-        result
+      case (None, None) => None
     }
   }
 
-  def getMiddle(node: Node): Option[Node] = {
+  /**
+    * Finds the middle of the SinglyLinkedList.
+    *
+    * @param node head of the linked list
+    * @return middle node
+    */
+  def findMiddle(node: Node): Option[Node] = {
     var fastPointer: Option[Node] = node.next
     var slowPointer: Option[Node] = Option(node)
 
