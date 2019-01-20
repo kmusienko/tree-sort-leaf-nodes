fc40d470eaf6c1a334fa3a84346d53304efb17b3
kmusienko <konstantin.musienko@sysgears.com>
2019-01-20 19:54:13 +0200

    Add scala-docs for TreeTraversalTypes

--- src/main/scala/TreeTraversalTypes.scala
+++ src/main/scala/TreeTraversalTypes.scala
@@ -1,12 +1,32 @@
 import scala.collection.immutable.Queue
 
+/**
+  * An object where tree traversal types are defined.
+  */
 object TreeTraversalTypes {
 
+  /**
+    * Trait that declares an iterator by which a tree can be traversed.
+    */
   sealed trait TraversalType {
+
+    /**
+      * Returns an iterator by which a tree can be traversed.
+      *
+      * @param node tree node to start traverse from
+      * @param f    function that takes a parent Node as an argument and returns a Queue containing all its children
+      * @tparam Node type of the tree node
+      * @return iterator by which a tree can traversed.
+      */
     def iterator[Node](node: Node, f: Node => Queue[Node]): Iterator[Node]
   }
 
+  /**
+    * The Breadth First Traversal type with provided iterator.
+    */
   case object BreadthFirst extends TraversalType {
+
+    /** @inheritdoc*/
     override def iterator[Node](node: Node, f: Node => Queue[Node]): Iterator[Node] = {
       def recurse(q: Queue[Node]): List[Node] = {
         if (q.isEmpty) List.empty
@@ -19,4 +39,5 @@ object TreeTraversalTypes {
       (node :: recurse(Queue.empty ++ f(node))).toIterator
     }
   }
+
 }
