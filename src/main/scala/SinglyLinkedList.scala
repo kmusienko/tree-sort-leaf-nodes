e77ab46eaf6a47ec490e30bead800768c3fbe93d
kmusienko <konstantin.musienko@sysgears.com>
2019-01-20 14:07:58 +0200

    Implement an ability to add a sequence of elements to SinglyLinkedList

--- src/main/scala/SinglyLinkedList.scala
+++ src/main/scala/SinglyLinkedList.scala
@@ -12,6 +12,21 @@ case class SinglyLinkedList() {
     override def toString: String = String.valueOf(value) + next.fold("")(_.toString)
   }
 
+  def this(elems: Int*) = {
+    this()
+    addAll(elems)
+  }
+
+  def addAll(elems: Seq[Int]): Unit = {
+    elems.foreach {
+      elem =>
+        val newNode = Node(elem)
+        newNode.next = head
+        head = Some(newNode)
+    }
+    size += elems.size
+  }
+
   def print(): Unit = head.foreach(_.print())
 
   override def toString: String = head.fold("Empty")(_.toString)
