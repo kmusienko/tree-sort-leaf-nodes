39f9e787ed9eb68238207d7555bcaae73e3dcf92
kmusienko <konstantin.musienko@sysgears.com>
2019-01-20 14:43:59 +0200

    Add tests that verify if leaf nodes are sorted correctly in corner cases

--- src/test/scala/TreeSpec.scala
+++ src/test/scala/TreeSpec.scala
@@ -6,6 +6,33 @@ import scala.collection.immutable.Queue
 class TreeSpec extends WordSpec with BeforeAndAfter with MustMatchers {
 
   "TreeSpec" must {
+    "sort leaf nodes in tree #1 where w = 0 (all leaf nodes should be removed)" in {
+      val tree =
+        TreeNode(
+          nodes = Queue(
+            TreeNode(
+              leafList = new SinglyLinkedList(2, 1, 4, 3)
+            ),
+            TreeNode(
+              nodes = Queue(
+                TreeNode(
+                  leafList = new SinglyLinkedList(1, 1)
+                )
+              ),
+              leafList = new SinglyLinkedList(2)
+            )
+          )
+        )
+
+      tree.sortLeafNodes(traversalType = BreadthFirst, w = 0)
+
+      tree.leafList mustBe empty
+      tree.nodes.head.leafList mustBe empty
+      tree.nodes(1).leafList mustBe empty
+      tree.nodes(1).nodes.head.leafList mustBe empty
+      tree.nodes(1).nodes.head.nodes mustBe empty
+    }
+
     "sort leaf nodes in tree #1 where w = 5" in {
       val tree =
         TreeNode(
@@ -60,6 +87,52 @@ class TreeSpec extends WordSpec with BeforeAndAfter with MustMatchers {
       tree.nodes(1).nodes.head.nodes mustBe empty
     }
 
+    "sort leaf nodes in tree #1 where w = 100 (the tree shouldn't be changed except sorting leaf nodes)" in {
+      val tree =
+        TreeNode(
+          nodes = Queue(
+            TreeNode(
+              leafList = new SinglyLinkedList(2, 1, 4, 3)
+            ),
+            TreeNode(
+              nodes = Queue(
+                TreeNode(
+                  leafList = new SinglyLinkedList(1, 1)
+                )
+              ),
+              leafList = new SinglyLinkedList(2)
+            )
+          )
+        )
+
+      tree.sortLeafNodes(traversalType = BreadthFirst, w = 100)
+
+      tree.leafList mustBe empty
+      tree.nodes.head.leafList.toScalaList() mustBe List(1, 2, 3, 4)
+      tree.nodes(1).leafList.toScalaList() mustBe List(2)
+      tree.nodes(1).nodes.head.leafList.toScalaList() mustBe List(1, 1)
+      tree.nodes(1).nodes.head.nodes mustBe empty
+    }
+
+    "sort leaf nodes in tree #2 where w = 0 (all leaf nodes should be removed)" in {
+      val tree =
+        TreeNode(
+          nodes = Queue(
+            TreeNode(),
+            TreeNode(),
+            TreeNode(),
+          ),
+          leafList = new SinglyLinkedList(3, 4, 1, 2)
+        )
+
+      tree.sortLeafNodes(traversalType = BreadthFirst, w = 0)
+
+      tree.leafList mustBe empty
+      tree.nodes.head.leafList mustBe empty
+      tree.nodes(1).leafList mustBe empty
+      tree.nodes(2).leafList mustBe empty
+    }
+
     "sort leaf nodes in tree #2 where w = 5" in {
       val tree =
         TreeNode(
@@ -94,6 +167,25 @@ class TreeSpec extends WordSpec with BeforeAndAfter with MustMatchers {
 
       tree.leafList.toScalaList() mustBe List(1, 2)
       tree.nodes.head.leafList.toScalaList() mustBe List(3)
+      tree.nodes(1).leafList mustBe empty
+      tree.nodes(2).leafList mustBe empty
+    }
+
+    "sort leaf nodes in tree #2 where w = 100 (the tree shouldn't be changed except sorting leaf nodes)" in {
+      val tree =
+        TreeNode(
+          nodes = Queue(
+            TreeNode(),
+            TreeNode(),
+            TreeNode(),
+          ),
+          leafList = new SinglyLinkedList(3, 4, 1, 2)
+        )
+
+      tree.sortLeafNodes(traversalType = BreadthFirst, w = 100)
+
+      tree.leafList.toScalaList() mustBe List(1, 2, 3, 4)
+      tree.nodes.head.leafList mustBe empty
       tree.nodes(1).leafList mustBe empty
       tree.nodes(2).leafList mustBe empty
     }
