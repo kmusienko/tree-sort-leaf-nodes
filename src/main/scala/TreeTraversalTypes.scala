b7ed7543c0494a22a1191743ce81a56b45d70fc5
kmusienko <konstantin.musienko@sysgears.com>
2019-01-20 14:31:14 +0200

    Implement an iterator for Breadth First Traversal of a tree

--- src/main/scala/TreeTraversalTypes.scala
+++ src/main/scala/TreeTraversalTypes.scala
@@ -5,4 +5,18 @@ object TreeTraversalTypes {
   sealed trait TraversalType {
     def iterator[Node](node: Node, f: Node => Queue[Node]): Iterator[Node]
   }
+
+  case object BreadthFirst extends TraversalType {
+    override def iterator[Node](node: Node, f: Node => Queue[Node]): Iterator[Node] = {
+      def recurse(q: Queue[Node]): List[Node] = {
+        if (q.isEmpty) List.empty
+        else {
+          val (node, tail) = q.dequeue
+          node :: recurse(tail ++ f(node))
+        }
+      }
+
+      (node :: recurse(Queue.empty ++ f(node))).toIterator
+    }
+  }
 }
